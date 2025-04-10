c1dbf1dee3e5f8a90b91eb52e14df364829ebfac
Gotlop <haitayo750@gmail.com>
2025-04-10 14:09:07 +0700

    Update route.ts

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,26 +1,18 @@
-export async function GET() {
-  const appUrl = process.env.NEXT_PUBLIC_URL;
-
-  const config = {
-    accountAssociation: {
-      header:
-        "eyJmaWQiOjM2MjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyY2Q4NWEwOTMyNjFmNTkyNzA4MDRBNkVBNjk3Q2VBNENlQkVjYWZFIn0",
-      payload: "eyJkb21haW4iOiJmcmFtZXMtdjIudmVyY2VsLmFwcCJ9",
-      signature:
-        "MHhiNDIwMzQ1MGZkNzgzYTExZjRiOTllZTFlYjA3NmMwOTdjM2JkOTY1NGM2ODZjYjkyZTAyMzk2Y2Q0YjU2MWY1MjY5NjI5ZGQ5NTliYjU0YzEwOGI4OGVmNjdjMTVlZTdjZDc2YTRiMGU5NzkzNzA3YzkxYzFkOWFjNTg0YmQzNjFi",
-    },
-    frame: {
-      version: "1",
-      name: "Blepblopia Translator",
-      iconUrl: `${appUrl}/icon.png`,
-      homeUrl: appUrl,
-      imageUrl: `${appUrl}/og-image.png`,
-      buttonTitle: "Translatee",
-      splashImageUrl: `${appUrl}/splash.png`,
-      splashBackgroundColor: "#a1a1ff",
-      webhookUrl: `${appUrl}/api/webhook`,
-    },
-  };
-
-  return Response.json(config);
+{
+  "accountAssociation": {
+    "header": "eyJmaWQiOjM2MjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyY2Q4NWEwOTMyNjFmNTkyNzA4MDRBNkVBNjk3Q2VBNENlQkVjYWZFIn0",
+    "payload": "eyJkb21haW4iOiJmcmFtZXMtdjIudmVyY2VsLmFwcCJ9",
+    "signature": "MHhiNDIwMzQ1MGZkNzgzYTExZjRiOTllZTFlYjA3NmMwOTdjM2JkOTY1NGM2ODZjYjkyZTAyMzk2Y2Q0YjU2MWY1MjY5NjI5ZGQ5NTliYjU0YzEwOGI4OGVmNjdjMTVlZTdjZDc2YTRiMGU5NzkzNzA3YzkxYzFkOWFjNTg0YmQzNjFi"
+  },
+  "frame": {
+    "version": "1",
+    "name": "Blepblopia Translator",
+    "iconUrl": "https://blpblopia-translator.vercel.app/icon.png",
+    "imageUrl": "https://blpblopia-translator.vercel.app/og-image.png",
+    "buttonTitle": "Translate",
+    "splashImageUrl": "https://blpblopia-translator.vercel.app/splas.png",
+    "splashBackgroundColor": "#a1a1ff",
+    "webhookUrl": "https://blpblopia-translator.vercel.app/api/webhook",
+    "homeUrl": "https://blpblopia-translator.vercel.app/"
+  }
 }
