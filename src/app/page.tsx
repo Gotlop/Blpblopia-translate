af31899f30307d192432256ecc6573956afe166f
Gotlop <haitayo750@gmail.com>
2025-04-10 13:03:44 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ const frame = {
   version: "next",
   imageUrl: `${appUrl}/opengraph-image`,
   button: {
-    title: "Launch Frame",
+    title: "Blepblopia Translator",
     action: {
       type: "launch_frame",
-      name: "Farcaster Frames v2 Demo",
+      name: "translate",
       url: appUrl,
       splashImageUrl: `${appUrl}/splash.png`,
       splashBackgroundColor: "#f7f7f7",
@@ -22,10 +22,10 @@ export const revalidate = 300;
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "Farcaster Frames v2 Demo",
+    title: "Blepblopia Translator",
     openGraph: {
-      title: "Farcaster Frames v2 Demo",
-      description: "A Farcaster Frames v2 demo app.",
+      title: "Blepblopia Translator",
+      description: "Translate your text into Blepblopia language",
     },
     other: {
       "fc:frame": JSON.stringify(frame),
